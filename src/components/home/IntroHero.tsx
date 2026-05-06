deb76eb6353e8b5b52003a90d30c9cd500c56ad2
jungwj09 <woox1jin@gmail.com>
2026-05-07 00:01:43 +0900

    feat: add main page

---
+++ src/components/home/IntroHero.tsx
@@ -0,0 +1,19 @@
+export default function IntroHero() {
+  return (
+    <section className="w-full max-w-97.5 mx-auto px-4 pt-4">
+      <div className="flex items-center mb-4">
+        <h1 className="text-2xl font-semibold text-black">
+          <span className="text-(--sunrin-blue) font-bold">선린인터넷고등학교 진로체험</span>에
+          <br />
+          오신 여러분을 진심으로 환영합니다
+        </h1>
+      </div>
+
+      <p className="text-base text-black">
+        몇 가지 질문을 통해 나에게 맞는
+        <br />
+        학과 또는 전공 동아리를 탐색해볼 수 있어요.
+      </p>
+    </section>
+  );
+}
---
+++ src/components/shared/YearBadge.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+interface AppHeaderProps {
+  align?: "left" | "center";
+}
+
+export default function AppHeader({
+  align = "center",
+}: AppHeaderProps) {
+  return (
+    <div
+      className={`w-full px-4 pt-12.5 pb-2 flex ${
+        align === "center" ? "justify-center" : "justify-start"
+      }`}
+    >
+      <div className="p-0.5 rounded-full bg-[linear-gradient(90deg,var(--sunrin-green)_0%,var(--sunrin-blue)_50%,var(--sunrin-red)_100%)]">
+        <div className="inline-flex items-center gap-1.5 rounded-full bg-white px-3.75 py-2">
+          <Image
+            src="/sunrin.png"
+            alt="sunrin"
+            width={22}
+            height={22}
+            className="shrink-0"
+          />
+
+          <span className="text-base font-semibold text-black tracking-tight">
+            2026 선린인터넷고 진로체험
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
